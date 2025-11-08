4e22f414feab3f4c47c4d23cfebef21d7dc90940
jiwoo <violet060913@gmail.com>
2025-11-09 00:48:41 +0900

    Update and rename lab10.py to divisors.py

---
+++ divisors.py
@@ -0,0 +1,19 @@
+import sys
+import math
+
+def divisors(n: int):
+    small, large = [], []
+    limit = int(math.isqrt(n))
+    for d in range(1, limit + 1):
+        if n % d == 0:
+            small.append(d)
+            if d != n // d:
+                large.append(n // d)
+    return small + large[::-1]  # 오름차순
+
+def main():
+    n = int(sys.argv[1])  # autograding이 인자로 줌 (예: 100, 23)
+    print(" ".join(map(str, divisors(n))))
+
+if __name__ == "__main__":
+    main()
